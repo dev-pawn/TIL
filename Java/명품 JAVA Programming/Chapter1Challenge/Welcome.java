ecfac3368f76d3052bb0934365e8ca26181bafd1
dev-pawn <dev_pawn@naver.com>
2022-04-30 18:00:43 +0900

    Create Welcome.java

---
+++ Java/명품 JAVA Programming/Chapter1Challenge/Welcome.java
@@ -0,0 +1,12 @@
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("환영합니다!");
+		System.out.println("첫 자바 코드 작성을 축하드립니다.");
+
+	}
+
+}
