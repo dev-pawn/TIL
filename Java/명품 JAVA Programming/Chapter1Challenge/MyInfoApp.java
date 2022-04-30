6520057901b7e1848fc21e827ce66d5f31e273e7
dev-pawn <dev_pawn@naver.com>
2022-04-30 17:51:56 +0900

    Create MyInfoApp.java

---
+++ Java/명품 JAVA Programming/Chapter1Challenge/MyInfoApp.java
@@ -0,0 +1,12 @@
+
+public class MyInfoApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Kim Ho");
+		System.out.println("30 years old");
+		System.out.println("Department of Software Engineering");
+	}
+
+}
