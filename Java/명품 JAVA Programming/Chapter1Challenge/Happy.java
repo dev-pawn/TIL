cafd3c657ee9215193756be51bf64d8199feb521
dev-pawn <dev_pawn@naver.com>
2022-04-30 18:00:51 +0900

    Create Happy.java

---
+++ Java/명품 JAVA Programming/Chapter1Challenge/Happy.java
@@ -0,0 +1,12 @@
+
+public class Happy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Happy~!");
+		System.out.println("행복한 하루 되세요~");
+
+	}
+
+}
