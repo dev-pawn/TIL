a00237fd2314d0f179e4fb7339e3419f2df4b844
dev_pawn <dev_pawn@naver.com>
2022-09-28 21:21:19 +0900

    First commit

---
+++ Python/NomadcoderWebscrapperChallenge/calculator.py
@@ -0,0 +1,25 @@
+def plus(num1=0, num2=0):
+    print(num1+num2)
+
+
+def minus(num1=0, num2=0):
+    print(num1-num2)
+
+
+def multiply(num1=0, num2=0):
+    print(num1*num2)
+
+
+def divide(num1=0, num2=1):
+    print(num1/num2)
+
+
+def power_of(num1=0):
+    print(num1*num1)
+
+
+plus(2, 3)
+minus(3, 2)
+multiply(2, 3)
+divide(3, 2)
+power_of(2)
