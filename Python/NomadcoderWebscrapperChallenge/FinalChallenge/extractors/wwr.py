603d896fb5e671cc3f5184a367881e1bc3354c08
dev_pawn <dev_pawn@naver.com>
2022-10-09 18:44:22 +0900

    First commit

--- Python/NomadcoderWebscrapperChallenge/FinalChallenge/extractors/wwr.py
+++ Python/NomadcoderWebscrapperChallenge/FinalChallenge/extractors/wwr.py
@@ -1,40 +1,49 @@
 from requests import get
 from bs4 import BeautifulSoup
 
-
 def extract_wwr_jobs(keyword):
-    base_url = "https://weworkremotely.com/remote-jobs/search?term="
-    response = get(f"{base_url}{keyword}")
+  url = f"https://weworkremotely.com/remote-jobs/search?term={keyword}"
+  request = get(url)
 
-    if response.status_code != 200:
-        print("Can't request website")
-    else:
-        results = []
-        soup = BeautifulSoup(response.text, "html.parser")
-        jobs = soup.find_all('section', class_="jobs")
-        for job_section in jobs:
-            job_posts = job_section.find_all('li')
-            job_posts.pop(-1)
-            for post in job_posts:
-                anchors = post.find_all('a')
-                anchor = anchors[1]
-                link = anchor['href']
-                companys = anchor.find_all('span', class_="company")
-                if len(companys) == 3:
-                    company, empty, region = companys
-                elif len(companys) == 2:
-                    company, region = companys
-                title = anchor.find('span', class_='title')
-                if region.string is None:
-                    print("Check None")
-                    region = "None"
-                else:
-                    region = region.string
-                job_data = {
-                    'link': f"https://weworkremotely.com{link}",
-                    'company': company.string.replace(",", " "),
-                    'location': region.replace(",", " "),
-                    'position': title.string.replace(",", " ")
-                }
-                results.append(job_data)
+  if request.status_code == 200:
+    results = []
+    soup = BeautifulSoup(request.text, "html.parser")
+    jobs = soup.find_all('section', class_="jobs")
+    for job_section in jobs:
+      job_posts = job_section.find_all('li')
+      job_posts.pop(-1)
+      for post in job_posts:
+        anchors = post.find_all('a')
+        anchor = anchors[1]
+        link = anchor['href']
+        spans = anchor.find_all('span', class_="company")
+        if len(spans) == 3:
+          company, empty, region = spans
+        elif len(spans) == 2:
+          company, region = spans
+        title = anchor.find('span', class_='title')
+        if company.string is None:
+          print("Check None")
+          company = "Empty"
+        else:
+          company = company.string
+        if region.string is None:
+          print("Check None")
+          region = "Empty"
+        else:
+          region = region.string
+        if title.string is None:
+          print("Check None")
+          title = "Empty"
+        else:
+          title = title.string
+        job_data = {
+          'link': f"https://weworkremotely.com{link}",
+          'company': company.replace(",", " "),
+          'location': region.replace(",", " "),
+          'position': title.replace(",", " ")
+        }
+        results.append(job_data)
     return results
+  else:
+    print(f"ERROR : status_code{request.status_code}")
--- Python/NomadcoderWebscrapperChallenge/FinalChallenge/main.py
+++ Python/NomadcoderWebscrapperChallenge/FinalChallenge/main.py
@@ -3,30 +3,27 @@ from extractors.remoteok import extract_remoteok_jobs
 from extractors.wwr import extract_wwr_jobs
 
 app = Flask("JobScrapper")
-app = Flask(__name__, template_folder='templates')
+#app = Flask(__name__, template_folder='templates')
 
 db = {}
 
-
 @app.route("/")
 def home():
-    return render_template("home.html")
-
+  return render_template("home.html")
 
 @app.route("/search")
 def search():
-    keyword = request.args.get("keyword")
-    if (keyword == "") or (keyword == None):
-        return redirect("/")
-    if keyword in db:
-        jobs = db[keyword]
-    else:
-        print("pass if")
-        remoteok = extract_remoteok_jobs(keyword)
-        wwr = extract_wwr_jobs(keyword)
-        jobs = remoteok + wwr
-        db[keyword] = jobs
-    return render_template("search.html", keyword=keyword, jobs=jobs)
+  keyword = request.args.get("keyword")
+  if (keyword == "") or (keyword == None):
+    return redirect("/")
+  if keyword in db:
+    jobs = db[keyword]
+  else:
+    remoteok = extract_remoteok_jobs(keyword)
+    wwr = extract_wwr_jobs(keyword)
+    jobs = remoteok + wwr
+    db[keyword] = jobs
+  return render_template("search.html", keyword=keyword, jobs=jobs)
 
 
 app.run("0.0.0.0")
