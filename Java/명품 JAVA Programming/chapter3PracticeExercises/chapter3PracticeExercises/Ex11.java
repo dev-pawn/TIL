4409869988455e980b4e473dc36fbf8ee5265206
dev-pawn <dev_pawn@naver.com>
2023-10-13 23:09:57 +0900

    first commit

---
+++ Java/명품 JAVA Programming/chapter3PracticeExercises/chapter3PracticeExercises/Ex11.java
@@ -0,0 +1,10 @@
+/*
+다음과 같이 작동하는 Average.java를 작성하라. 명령행 인자는 모두 정수만 사용되며 정수들의 평균을 출력한다.
+다음 화면은 컴파일된 Average.class 파일을 c:\Temp 디렉토리에 복사한 뒤 실행한 경우이다.
+
+c:\Temp>java Average 2 3 4
+3
+c:\Temp>
+ */
+public class Ex11 {
+}
