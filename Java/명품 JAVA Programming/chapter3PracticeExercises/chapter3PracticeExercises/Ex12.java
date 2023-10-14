aa7bf5314ca0a17957ea64f71a27e49c39733378
dev-pawn <dev_pawn@naver.com>
2023-10-14 18:29:47 +0900

    first commit

---
+++ Java/명품 JAVA Programming/chapter3PracticeExercises/chapter3PracticeExercises/Ex12.java
@@ -0,0 +1,16 @@
+/*
+다음과 같이 작동하는 Add.java를 작성하라. 명령행 인자 중에서 정수 만을 골라 합을 구하라.
+다음 화면은 Add.class 파일을 c:\Temp 디렉터리에 복사한 뒤 실행한 경우이다.
+C:\Temp>java Add 2 3 aa 5 6.5
+10
+
+C:\Temp>java Add Hell 3 4 4.3 c
+7
+
+C:\Temp>
+ */
+public class Ex12 {
+    public static void main(String[] args) {
+
+    }
+}
