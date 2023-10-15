ada7cd05e44b00d7c97bef68a1fbcd59200bd9a9
dev-pawn <dev_pawn@naver.com>
2023-10-15 23:03:53 +0900

    first commit

---
+++ Java/명품 JAVA Programming/chapter3PracticeExercises/chapter3PracticeExercises/Ex15.java
@@ -0,0 +1,35 @@
+/*
+다음은 2개의 정수를 입력 받아 곱을 구하는 Multiply 클래스이다.
+import java.util.Scanner;
+public class Multiply {
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("곱하고자하는 두 수 입력>>");
+    int n = scanner.nextInt();
+    int m = scanner.nextInt();
+    System.out.print(n + "x" + m + "=" + n*m);
+    scanner.close();
+    }
+}
+다음과 같이 실행할 때 프로그램은 10과 5를 곱해 50을 잘 출력한다.
+곱하고자 하는 두 수 입력>>10 5
+10x5=50
+하지만, 다음과 같이 실수를 입력하였을 때, 예외가 발생한다.
+곱하고자 하는 두 수 입력>>2.5 4
+Exception in thread "main" java.util.InputMismatchException
+    at java.util.Scanner.throwFor(Unknown Source)
+    at java.util.Scanner.next(Unknown Source)
+    at java.util.Scanner.nextInt(Unknown Source)
+    at java.util.Scanner.nextInt(Unknown Source)
+    at Multiply.main(Multiply.java:6)  //라인 6에서 예외 발생
+
+다음과 같이 실수가 입력되면 정수를 다시 입력하도록 하여 예외 없이 정상적으로 처리되도록 예외 처리 코드를 삽입하여 Multiply 클래스를 수정하라.
+곱하고자 하는 두 수 입력>>2.5 4
+실수는 입력하면 안됩니다.
+곱하고자 하는 두 수 입력>>4 3.5
+실수는 입력하면 안됩니다.
+곱하고자 하는 두 수 입력>>4 3
+4x3=12
+ */
+public class Ex15 {
+}
